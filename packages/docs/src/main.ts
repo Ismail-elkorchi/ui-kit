dd3ddc4802e1eea672e232d2a8527f2e09efafcf
Ismail El Korchi <ismail.elkorchi@gmail.com>
2026-02-02 00:16:23 +0100

    perf(docs): delay app mount until next frame

--- packages/docs/src/main.ts
+++ packages/docs/src/main.ts
@@ -5,7 +5,14 @@ if (!appRoot) {
   throw new Error("Docs root element not found.");
 }
 
-void mountDocsApp(appRoot);
+const startApp = () => {
+  void mountDocsApp(appRoot);
+};
+if ("requestAnimationFrame" in window) {
+  requestAnimationFrame(() => startApp());
+} else {
+  globalThis.setTimeout(startApp, 0);
+}
 
 const scheduleBreakpointObserver = () => {
   const run = () => {
